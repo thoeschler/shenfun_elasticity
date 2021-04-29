21e32154ed9f7aed17cda0de04ea1dfc3d4b568e
thoeschler <64902718+thoeschler@users.noreply.github.com>
2021-04-29 18:57:18 +0200

    Shortened error computation. Added doc string.

--- Solver/check_solution.py
+++ Solver/check_solution.py
@@ -1,7 +1,26 @@
-from shenfun import div, grad, Function, project, FunctionSpace, TensorProductSpace, Array, VectorSpace, inner, comm
-import math
+from shenfun import div, grad, Function, project, Array, inner
+from math import sqrt
 
 def check_solution_cauchy(u_hat, material_parameters, body_forces):
+    '''
+    Check wheter the numerically computed solution fulfills the Lamé-Navier equation.
+    
+    Parameters
+    ----------
+    u_hat : shenfun Function
+        Displacement in spectral space (expansion coefficients).
+    material_parameters : tuple or list
+        Lamé-parameters: (lambd, mu).
+    body_forces : tuple
+        Components of body forces as sympy expressions.
+
+    Returns
+    -------
+    error
+        L2 norm is computed to check whether the numerical solution fulfills the PDE.
+
+    '''
+    # assert input
     assert isinstance(u_hat, Function)
     assert isinstance(material_parameters, tuple)
     assert len(material_parameters) == 2
@@ -9,7 +28,6 @@ def check_solution_cauchy(u_hat, material_parameters, body_forces):
         assert isinstance(val, float)
     
     # some parameters
-    dim = len(u_hat)
     lambd = material_parameters[0]
     mu = material_parameters[1]
     
@@ -17,28 +35,41 @@ def check_solution_cauchy(u_hat, material_parameters, body_forces):
     lhs = (lambd + mu)*grad(div(u_hat)) + mu*div(grad(u_hat))
     
     # space for volumetric forces
-    tens_space = tuple(
-        [FunctionSpace(N=u_hat.function_space().spaces[i].bases[i].N, \
-                                   domain=u_hat.function_space().spaces[i].bases[i].domain, \
-                                       family='legendre', bc=None) for i in range(dim)]
-            )
-    T = TensorProductSpace(comm, tens_space)
-    V = VectorSpace([T, T])
+    V = u_hat.function_space().get_orthogonal()
     
-    # test solution using lame-navier-equation
+    # evaluate volumetric forces at quadrature points (physical space)
     error_array = Array(V, buffer=body_forces)  
-    for i in range(dim):
-        error_array[i] += project(lhs[i], T).backward()
     
-    for i in range(dim):
-        for j in range(len(error_array[i])):
-            for k in range(len(error_array[i][j])):
-                error_array[i][j][k] = error_array[i][j][k]**2
-    error = inner((1, 1), error_array)
-    return math.sqrt(error)
+    # add left hand side of the Lamé-Navier equation
+    error_array += project(lhs, V).backward()
+    
+    # compute integral error
+    error = sqrt(inner((1, 1), error_array**2))
+    
+    return error
     
 
 def check_solution_gradient(u_hat, material_parameters, body_forces):
+    '''
+    Check wheter the numerically computed solution fulfills the 
+    balance of linear momentum for linear gradient elasticity.
+    
+    Parameters
+    ----------
+    u_hat : shenfun Function
+        Displacement in spectral space (expansion coefficients).
+    material_parameters : tuple or list
+        Lamé-parameters: (lambd, mu, c1, c2, c3, c4, c5).
+    body_forces : tuple
+        Components of body forces as sympy expressions.
+
+    Returns
+    -------
+    error
+        L2 norm is computed to check whether the numerical solution fulfills the PDE.
+
+    '''
+    # assert input
     assert isinstance(u_hat, Function)
     assert isinstance(material_parameters, tuple)
     assert len(material_parameters) == 7
@@ -46,7 +77,6 @@ def check_solution_gradient(u_hat, material_parameters, body_forces):
         assert isinstance(val, float)
     
     # some parameters
-    dim = len(u_hat)
     lambd = material_parameters[0]
     mu = material_parameters[1]
     c1 = material_parameters[2]
@@ -60,20 +90,15 @@ def check_solution_gradient(u_hat, material_parameters, body_forces):
         (c1 + c4)*div(grad(div(grad(u_hat)))) - (c2 + c3 + c5)*grad(div(div(grad(u_hat))))
     
     # space for volumetric forces
-    bases = [FunctionSpace(N=u_hat.function_space().spaces[i].bases[i].N, \
-                                   domain=u_hat.function_space().spaces[i].bases[i].domain, \
-                                       family='legendre', bc=None) for i in range(dim)]
-    T = TensorProductSpace(comm, tuple(bases))
-    V = VectorSpace([T, T])
+    V = u_hat.function_space().get_orthogonal()
     
-    # test solution using lame-navier-equation
+    # evaluate volumetric forces at quadrature points (physical space)
     error_array = Array(V, buffer=body_forces)  
-    for i in range(dim):
-        error_array[i] += project(lhs[i], T).backward()
     
-    for i in range(dim):
-        for j in range(len(error_array[i])):
-            for k in range(len(error_array[i][j])):
-                error_array[i][j][k] = error_array[i][j][k]**2
-    error = inner((1, 1), error_array)
-    return math.sqrt(error)
+    # add left hand side of the Lamé-Navier equation
+    error_array += project(lhs, V).backward()
+    
+    # compute integral error
+    error = sqrt(inner((1, 1), error_array**2))
+    
+    return error
