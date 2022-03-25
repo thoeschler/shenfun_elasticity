9e219468595f60826f4726473b7e2a9d78050ad0
thoeschler <thilo.hoeschler@gmx.de>
2022-03-25 23:29:40 +0100

    add traction conditions for 3d case

--- shenfun_elasticity/solver/solver.py
+++ shenfun_elasticity/solver/solver.py
@@ -1,6 +1,7 @@
 import shenfun as sf
 import numpy as np
 from shenfun import inner, comm, Function, Array
+from operator import itemgetter
 
 
 class ElasticSolver:
@@ -34,6 +35,67 @@ class ElasticSolver:
 
         self._V = sf.VectorSpace(vec_space)
 
+    def _setup_traction_bcs(self):
+        boundary_traction_term = Function(self._V.get_orthogonal())
+
+        if self._dim == 2:
+            map_boundary_to_component_index = {'left': 0, 'right': 0,
+                                               'bottom': 1, 'top': 1}
+            map_boundary_to_start_end_index = {'left': -1, 'right': 1,
+                                               'bottom': -1, 'top': 1}
+        elif self._dim == 3:
+            map_boundary_to_component_index = {'left': 0, 'right': 0,
+                                               'back': 1, 'front': 1,
+                                               'bottom': 2, 'top': 2}
+            map_boundary_to_start_end_index = {'left': -1, 'right': 1,
+                                               'back': -1, 'front': 1,
+                                               'bottom': -1, 'top': 1}
+
+        # b: boundary, c: component
+        for b, c, value in self._traction_bcs:
+            side_index = map_boundary_to_component_index[b]
+            start_or_end_index = map_boundary_to_start_end_index[b]
+            if self._dim == 2:
+                bdry_basis_index = 1 if side_index == 0 else 0
+                boundary_basis = self._function_spaces[c][bdry_basis_index]
+                # test function for boundary integral
+                v_boundary = sf.TestFunction(boundary_basis)
+                if isinstance(value, (float, int)):
+                    trac = Array(boundary_basis, val=value)
+                else:
+                    trac = Array(boundary_basis, buffer=value)
+                evaluate_on_boundary = self._function_spaces[c][
+                    side_index].evaluate_basis_all(start_or_end_index)
+                project_traction = inner(trac, v_boundary)
+                if side_index == 0:
+                    boundary_traction_term[c] += np.outer(
+                        evaluate_on_boundary, project_traction)
+                elif side_index == 1:
+                    boundary_traction_term[c] += np.outer(
+                        project_traction, evaluate_on_boundary)
+                else:
+                    raise ValueError()
+            elif self._dim == 3:
+                bdry_basis_indices = list(range(3))
+                bdry_basis_indices.remove(side_index)
+                boundary_bases = list(itemgetter(*bdry_basis_indices)
+                                      (self._function_spaces[c]))
+                # test function for boundary integral
+                T = sf.TensorProductSpace(comm, boundary_bases)
+                v_boundary = sf.TestFunction(T)
+                if isinstance(value, (float, int)):
+                    trac = Array(T, val=value)
+                else:
+                    trac = Array(T, buffer=value)
+                evaluate_on_boundary = self._function_spaces[c][
+                    side_index].evaluate_basis_all(start_or_end_index)
+                project_traction = inner(trac, v_boundary)
+                boundary_traction_term[c] += np.swapaxes(
+                    np.tensordot(evaluate_on_boundary.squeeze(),
+                                 project_traction, axes=0),
+                    0, side_index)
+        self._dw_ext += boundary_traction_term
+
     def _setup_variational_problem(self):
         self._setup_function_space()
         u = sf.TrialFunction(self._V)
@@ -52,43 +114,10 @@ class ElasticSolver:
             self._dw_ext = inner(v, body_forces_quad)
 
     def solve(self):
-        if self._dim == 2:
-            map_boundary_to_component_index = {'right': 0, 'top': 1, 'left': 0,
-                                               'bottom': 1}
-            map_boundary_to_start_end_index = {'right': 1, 'top': 1,
-                                               'left': -1, 'bottom': -1}
         if self._traction_bcs:
-            boundary_traction_term = Function(self._V.get_orthogonal())
-
-            # b: boundary, c: component
-            for b, c, value in self._traction_bcs:
-                if self._dim == 2:
-                    side_index = map_boundary_to_component_index[b]
-                    bdry_basis_index = 1 if side_index == 0 else 0
-                    boundary_basis = self._function_spaces[c][bdry_basis_index]
-                    start_or_end_index = map_boundary_to_start_end_index[b]
-                    # test function for boundary integral
-                    v_boundary = sf.TestFunction(boundary_basis)
-                    if isinstance(value, (float, int)):
-                        trac = Array(boundary_basis, val=value)
-                    else:
-                        trac = Array(boundary_basis, buffer=value)
-                    evaluate_on_boundary = self._function_spaces[c][
-                        side_index].evaluate_basis_all(start_or_end_index)
-                    project_traction = inner(trac, v_boundary)
-                    if side_index == 0:
-                        boundary_traction_term[c] += np.outer(
-                            evaluate_on_boundary, project_traction)
-                    elif side_index == 1:
-                        boundary_traction_term[c] += np.outer(
-                            project_traction, evaluate_on_boundary)
-                    else:
-                        raise ValueError()
+            self._setup_traction_bcs()
 
         M = sf.BlockMatrix(self._dw_int)
-        if self._traction_bcs:
-            self._dw_ext += boundary_traction_term
         u_hat = M.solve(self._dw_ext)
-
 
         return u_hat
